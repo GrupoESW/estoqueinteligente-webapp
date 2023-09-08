13503bd7f8b1e14d92bf0162370c4a6b6c187e88
Nilton Junior <cezarbrumjr@gmail.com>
2023-09-08 13:39:20 -0300

    corrige urls

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    api_url = os.environ.get('LETSENCRYPT_HOST').replace('webapp', 'api')
+    return render_template('index.html', api_url=api_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
