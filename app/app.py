2e1ffce728c6c97f852f594af9e5a1642896e33a
Nilton Junior <cezarbrumjr@gmail.com>
2023-09-12 22:16:10 -0300

    cria página de login exemplo

--- app/app.py
+++ app/app.py
@@ -7,5 +7,9 @@ def home():
     api_url = os.environ.get('LETSENCRYPT_HOST').replace('webapp', 'api')
     return render_template('index.html', api_url=api_url)
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
