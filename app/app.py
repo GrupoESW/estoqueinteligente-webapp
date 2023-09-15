12d47410b82ceee1e556a6b083bf5ca5b38aa5d9
Nilton Junior <cezarbrumjr@gmail.com>
2023-09-15 16:58:59 -0300

    nova versão do dash

--- app/app.py
+++ app/app.py
@@ -11,6 +11,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dash.html')
+
 if __name__ == '__main__':
     port_number = 80
     app.run(debug=True, host='0.0.0.0', port=port_number)
